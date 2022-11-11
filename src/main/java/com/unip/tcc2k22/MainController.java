b7e3eafac6923c8d49a46f75bcc5688f7ccc81e2
Felipe Gabriel de Oliveira Milani <64813256+necrotg@users.noreply.github.com>
2022-11-10 21:27:21 -0300

    Added limit

--- src/main/java/com/unip/tcc2k22/MainController.java
+++ src/main/java/com/unip/tcc2k22/MainController.java
@@ -6,18 +6,18 @@ import com.unip.tcc2k22.models.DataSpring;
 import com.unip.tcc2k22.repositories.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/insert")
@@ -28,19 +28,24 @@ public class MainController {
 
     @GetMapping
     public String get(){
-
         return "OK";
     }
 
     @PostMapping
-    public String saveData() throws IOException {
+    public ResponseEntity<String> saveData(int limit) throws IOException {
          String text = readFile("C:\\Users\\fgmil\\IdeaProjects\\tcc2k22\\src\\main\\java\\com\\unip\\tcc2k22\\data.json");
-            long startTime = System.currentTimeMillis();
+
         ObjectMapper mapper = new ObjectMapper();
-        List<DataSpring> dataSprings = mapper.readValue(text, new TypeReference<List<DataSpring>>(){});
-            repository.saveAll(dataSprings);
+
+        List<DataSpring> dataSpring = mapper.readValue(text, new TypeReference<List<DataSpring>>(){});
+        List<DataSpring> dataToSave = new ArrayList<>();
+        for(int i = 0; i < limit;i++){
+            dataToSave.add(dataSpring.get(i));
+        }
+        long startTime = System.currentTimeMillis();
+            repository.saveAll(dataToSave);
             long endTime = System.currentTimeMillis();
-        return "That took " + (endTime - startTime) + " milliseconds";
+            return new ResponseEntity<>("That took " + (endTime - startTime) + " milliseconds saved "+limit, HttpStatus.OK);
     }
 
     public String readFile(String fileName) throws IOException {
